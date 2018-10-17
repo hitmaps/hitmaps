6e261d77645e53fd17d79b3d6c728fc2bbfc6a8d
Mike Koch <mkoch227@gmail.com>
2018-10-17 13:00:45 -0400

    Nodes can now be added directly via the map

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ $twig = new Twig_Environment($loader);
 
 $klein = new \Klein\Klein();
 
-$klein->respond('GET', '/', function() use ($twig, $applicationContext) {
+$klein->respond('GET', '/', function () use ($twig, $applicationContext) {
     return \Controllers\Renderer::render('game-select.twig', $twig);
 });
 
-$klein->respond('GET', '/[:game]', function(\Klein\Request $request) use ($twig, $applicationContext) {
+$klein->respond('GET', '/[:game]', function (\Klein\Request $request) use ($twig, $applicationContext) {
     if (!in_array($request->game, [\BusinessLogic\Game::HITMAN, \BusinessLogic\Game::HITMAN2])) {
         return http_response_code(404);
     }
@@ -21,7 +21,7 @@ $klein->respond('GET', '/[:game]', function(\Klein\Request $request) use ($twig,
     return \Controllers\Renderer::render("game/{$request->game}.twig", $twig, $gameViewModel);
 });
 
-$klein->respond('GET', '/[:game]/[:location]/[:missionSlug]/[:difficulty]', function(\Klein\Request $request) use ($twig, $applicationContext) {
+$klein->respond('GET', '/[:game]/[:location]/[:missionSlug]/[:difficulty]', function (\Klein\Request $request) use ($twig, $applicationContext) {
     $viewModel = new \ViewModels\GameMapViewModel();
     $viewModel->difficulty = $request->difficulty;
     $viewModel->game = $request->game;
@@ -37,14 +37,34 @@ $klein->respond('GET', '/[:game]/[:location]/[:missionSlug]/[:difficulty]', func
     return \Controllers\Renderer::render('map.twig', $twig, $viewModel);
 });
 
-$klein->respond('POST', '/api/nodes', function(\Klein\Request $request, \Klein\Response $response) use ($twig, $applicationContext) {
+$klein->respond('POST', '/api/nodes', function (\Klein\Request $request, \Klein\Response $response) use ($twig, $applicationContext) {
+    /*
+     * array (size=11)
+     * 'type' => string 'sabotage' (length=8)
+     * 'icon' => string 'explosion' (length=9)
+     * 'name' => string 'test' (length=4)
+     * 'target' => string 'test' (length=4)
+     * 'note-type' =>
+     *   array (size=2)
+     *     0 => string 'requirement' (length=11)
+     *     1 => string 'requirement' (length=11)
+     * 'note-text' =>
+     *   array (size=2)
+     *     0 => string '' (length=0)
+     *     1 => string '' (length=0)
+     * 'mission-id' => string '2' (length=1)
+     * 'level' => string '0' (length=1)
+     * 'latitude' => string '-95.375' (length=7)
+     * 'longitude' => string '113.5' (length=5)
+     * 'difficulty' => string 'standard' (length=8)
+     */
 
-    // TODO Add node
+    $applicationContext->get(\Controllers\NodeController::class)->createNode(intval($_POST['mission-id']), $_POST['difficulty'], $_POST);
 
     return $response->code(204);
 });
 
-$klein->respond('GET', '/api/nodes', function() use ($applicationContext) {
+$klein->respond('GET', '/api/nodes', function () use ($applicationContext) {
     return json_encode($applicationContext->get(\Controllers\NodeController::class)->getNodesForMission($_GET['missionId'], $_GET['difficulty']));
 });
 
