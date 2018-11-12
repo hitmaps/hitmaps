084d93e37dc449e1f1352c0a58d8167b047361d8
Mike Koch <mkoch227@gmail.com>
2018-11-11 20:54:32 -0500

    Working on CI

---
+++ ci/deploy_to_prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ../
+mkdir latest_build
+TARGETDIR=latest_build;for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
+
+# Remove files that we don't want to bundle
+cd latest_build
+rm -rf ci
+rm -rf .git
+rm -rf db
+
+zip -r ../latest_build.zip .
+cd ../
+rm -rf latest_build
+mkdir latest_build
+mv latest_build.zip latest_build
+
+lftp -d -e "set ssl:verify-certificate no; mirror -P $NUMBER_OF_CONCURRENT_UPLOADS -Rv $LOCAL_DIR $REMOTE_DIR; quit;" ftp://$USERNAME:$PASSWORD@$HOST
+
+wget https://www.hitman2maps.com/unzip-build.php > /dev/null
