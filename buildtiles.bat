fceba1fd2954d86874beee92188adda7b52f3076
Mike Koch <mkoch227@gmail.com>
2018-11-18 19:36:24 -0500

    Add all legacy pack tiles

---
+++ buildtiles.bat
@@ -0,0 +1,34 @@
+REM Sapienza
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\1.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\1"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\2.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\2"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\3.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\3"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\4.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\4"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\5.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\5"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\6.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\6"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\7.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Sapienza\tiles\7"
+
+REM Marrakesh
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\0.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\tiles\0"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\1.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\tiles\1"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\2.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\tiles\2"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\3.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Marrakesh\tiles\3"
+
+REM Bangkok
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\0.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\0"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\1.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\1"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\2.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\2"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\3.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\3"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\4.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\4"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\5.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Bangkok\tiles\5"
+
+REM Colorado
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\0.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\tiles\0"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\1.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\tiles\1"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\2.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\tiles\2"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\3.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Colorado\tiles\3"
+
+REM Hokkaido
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\0.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\tiles\0"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\1.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\tiles\1"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\2.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\tiles\2"
+gdal2tiles.py -l -p raster -z 0-5 -w none "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\3.png" "E:\Users\mkoch\Downloads\H2 Legacy Maps\Hokkaido\tiles\3"
