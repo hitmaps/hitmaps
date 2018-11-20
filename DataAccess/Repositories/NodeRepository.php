d8f173446078aa77cee20b206a210b2a93884cb9
Mike Koch <mkoch227@gmail.com>
2018-11-19 22:26:55 -0500

    Only return approved nodes, delete zip after deploy

--- DataAccess/Repositories/NodeRepository.php
+++ DataAccess/Repositories/NodeRepository.php
@@ -7,6 +7,6 @@ use Doctrine\ORM\EntityRepository;
 
 class NodeRepository extends EntityRepository {
     public function findByMissionAndDifficulty(int $missionId, string $difficulty): array {
-        return $this->findBy(['missionId' => $missionId, 'difficulty' => $difficulty]);
+        return $this->findBy(['missionId' => $missionId, 'difficulty' => $difficulty, 'approved' => true]);
     }
 }
--- unzip-build.php
+++ unzip-build.php
@@ -1,2 +1,3 @@
 <?php
 shell_exec('unzip -o latest_build.zip');
+shell_exec('rm latest_build.zip');
