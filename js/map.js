cb26a3332b6040cb92722abf84db94624e552bcb
Mike Koch <mkoch227@gmail.com>
2018-12-14 22:48:34 -0500

    #24 Improve the editor. Still need to support deleting ledges

--- js/map.js
+++ js/map.js
@@ -6,26 +6,47 @@ $(document).ready(function() {
     };
 
     $('#edit-button').click(function() {
-        $('.edit-menu').show();
-        $('#accordion').hide();
+        var $editMenu = $('.edit-menu');
+        if ($editMenu.is(':visible')) {
+            $editMenu.hide();
+            $('#accordion').show();
+        } else {
+            $editMenu.show();
+            $('#accordion').hide();
+        }
         $('[data-toggle="tooltip"]').tooltip('hide');
     });
 
     $('[data-editor="items"]').click(function() {
         var $editMode = $('input[type="hidden"][name="edit-mode"]');
         $editMode.val(EDIT_MODES.ITEMS);
-        toastr["info"]('Click anywhere to create a new node.');
         $('.editor-enabled').show().find('h3').text('ADD / REMOVE ITEMS');
         $('.edit-menu').hide();
+        $('.items-menu').show();
+    });
+
+    $('[data-item="cancel"]').click(function() {
+        var $editMode = $('input[type="hidden"][name="edit-mode"]');
+        $editMode.val(EDIT_MODES.OFF);
+        $('.edit-menu').show();
+        $('.items-menu').hide();
+        $('.editor-enabled').hide();
     });
 
     $('[data-editor="ledges"]').click(function() {
         var $editMode = $('input[type="hidden"][name="edit-mode"]');
         $editMode.val(EDIT_MODES.LEDGES);
-        toastr["info"]('Use the buttons below to create / delete ledges.');
         $('.editor-enabled').show().find('h3').text('ADD / REMOVE LEDGES');
         $('.edit-menu').hide();
         $('.ledges-menu').show();
+    });
+
+    $('[data-ledge="cancel"]').click(function() {
+        var $editMode = $('input[type="hidden"][name="edit-mode"]');
+        $editMode.val(EDIT_MODES.OFF);
+        $('.edit-menu').show();
+        $('.ledges-menu').hide();
+        $('.editor-enabled').hide();
     });
 
     $('.search-box').find('select').change(function() {
