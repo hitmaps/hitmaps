53a0100080f1501093dfd32afe43941e4fffdbd6
Mike Koch <Mike.Koch@emerson.com>
2019-01-14 13:05:48 -0500

    #11 Add Miami disguises
    Need to add images though

---
+++ db/migrations/20190114175838_add_disguises_to_miami.php
@@ -0,0 +1,162 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddDisguisesToMiami extends AbstractMigration {
+    public function up() {
+
+    }
+
+    private function rows() {
+        return [
+            // Common
+            [
+                'mission_id' => 22,
+                'name' => 'Event Crew',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Event Security',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Food Vendor',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kronstadt Engineer',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kronstadt Security',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Medic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Race Marshall',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Waiter',
+                'image' => '',
+            ],
+            // Uncommon
+            [
+                'mission_id' => 22,
+                'name' => 'Journalist',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kitchen Staff',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kronstadt Researcher',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Mascot',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Race Coordinator',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Aeon Mechanic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kowoon Mechanic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kronstadt Mechanic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Sotteraneo Mechanic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Thwack Mechanic',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Florida Man',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Moses Lee',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Pale Rider',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Sheik',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Street Musician',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Ted Mendez',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Aeon Driver',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Blue Seed Driver',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Crashed Kronstadt Driver',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Kowoon Driver',
+                'image' => '',
+            ],
+            [
+                'mission_id' => 22,
+                'name' => 'Thwack Driver',
+                'image' => '',
+            ],
+        ];
+    }
+}
