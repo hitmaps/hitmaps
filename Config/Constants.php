fba2850dd3b796951f0ad33dd72cd7fa82d6cc31
Mike Koch <mkoch227@gmail.com>
2019-03-04 22:10:49 -0500

    Add commit hash to query string to prevent caching

--- Config/Constants.php
+++ Config/Constants.php
@@ -6,8 +6,10 @@ namespace Config;
 class Constants {
     public $siteDomain = 'https://www.hitman2maps.com';
     public $currentYear;
+    public $cacheHash;
 
     public function __construct() {
         $this->currentYear = date('Y');
+        $this->cacheHash = file_get_contents(__DIR__ . '/cachehash.txt');
     }
 }
