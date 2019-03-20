d0d25c414452855b8667da8c5af7055eb7af6878
Mike Koch <mkoch227@gmail.com>
2019-03-19 20:25:21 -0400

    Add 500 error page

--- Controllers/Renderer.php
+++ Controllers/Renderer.php
@@ -5,9 +5,14 @@ namespace Controllers;
 
 use Config\Constants;
 use Config\Settings;
+use Controllers\ViewModels\BaseModel;
 
 class Renderer {
     static function render($view, \Twig_Environment $twig, $model = null) {
+        if ($model === null) {
+            $model = new BaseModel();
+        }
+
         return $twig->render($view, ['model' => $model, 'constants' => new Constants(), 'settings' => new Settings()]);
     }
 }
