821a053783e4d328947503fc7eebc6435b60fc7c
DeeJayBro <info@deejaybro.de>
2019-04-28 09:20:00 +0200

    Move all images out of /cdn and into /img

--- api/BusinessLogic/CountdownComposer.php
+++ api/BusinessLogic/CountdownComposer.php
@@ -7,12 +7,12 @@ use DataAccess\Models\ElusiveTarget;
 class CountdownComposer {
     public function composeElusiveTargetImage(ElusiveTarget $elusiveTarget, int $numberOfDays): string {
         $dayOrDays = $numberOfDays === 1 ? 'day' : 'days';
-        $overlay = new \Imagick(__DIR__ . "/../cdn/png/elusive-target-overlays/countdown/{$numberOfDays}-{$dayOrDays}-remaining.png");
-        $originalImage = new \Imagick(__DIR__ . "/../cdn/jpg{$elusiveTarget->getImageUrl()}.jpg");
+        $overlay = new \Imagick(__DIR__ . "/../../public/img/png/elusive-target-overlays/countdown/{$numberOfDays}-{$dayOrDays}-remaining.png");
+        $originalImage = new \Imagick(__DIR__ . "/../../public/img/jpg{$elusiveTarget->getImageUrl()}.jpg");
 
         $originalImage->compositeImage($overlay, \Imagick::COMPOSITE_DEFAULT, 0, 0);
-        $compositeUrl = "/cdn/jpg/elusive-targets/countdowns/{$elusiveTarget->getId()}-{$numberOfDays}.jpg";
-        $originalImage->setImageFilename(__DIR__ . "/..{$compositeUrl}");
+        $compositeUrl = "/img/jpg/elusive-targets/countdowns/{$elusiveTarget->getId()}-{$numberOfDays}.jpg";
+        $originalImage->setImageFilename(__DIR__ . "/../../public{$compositeUrl}");
         $originalImage->writeImage();
 
         $overlay->destroy();
@@ -22,7 +22,7 @@ class CountdownComposer {
     }
 
     public function deleteAllCompositeImages(ElusiveTarget $elusiveTarget): void {
-        $compositeDirectory = __DIR__ . '/../cdn/jpg/elusive-targets/countdowns';
+        $compositeDirectory = __DIR__ . '/../../public/img/jpg/elusive-targets/countdowns';
         @unlink("{$compositeDirectory}/{$elusiveTarget->getId()}-7.jpg");
         @unlink("{$compositeDirectory}/{$elusiveTarget->getId()}-5.jpg");
         @unlink("{$compositeDirectory}/{$elusiveTarget->getId()}-3.jpg");
