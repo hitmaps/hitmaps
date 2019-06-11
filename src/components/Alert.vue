ca0ed129b8186b8e27abf30de36c100a3f6a1b55
Mike Koch <Mike.Koch@emerson.com>
2019-06-11 13:03:04 -0400

    Add Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="alert" :class="'alert-' + type">
+        <h4 class="alert-heading" v-if="alertHeading !== undefined" v-html="alertHeading"></h4>
+        <slot></slot>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" v-if="dismissable">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Alert",
+        props: {
+            type: String,
+            alertHeading: String,
+            dismissable: Boolean
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .alert {
+        border: none;
+        border-radius: 0;
+
+        &.alert-success {
+            border-left: solid 6px green;
+        }
+    }
+</style>
