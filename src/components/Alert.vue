030d6905edaad63e643b532be842d90fe2030cd7
Mike Koch <mkoch227@gmail.com>
2019-06-11 21:56:39 -0400

    Display alerts on the login and register pages

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,31 +1,45 @@
 <template>
     <div class="alert" :class="'alert-' + type">
-        <h4 class="alert-heading" v-if="alertHeading !== undefined" v-html="alertHeading"></h4>
+        <h4
+            class="alert-heading"
+            v-if="alertHeading !== undefined"
+            v-html="alertHeading"
+        ></h4>
         <slot></slot>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close" v-if="dismissable">
+        <button
+            type="button"
+            class="close"
+            data-dismiss="alert"
+            aria-label="Close"
+            v-if="dismissable"
+        >
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Alert",
-        props: {
-            type: String,
-            alertHeading: String,
-            dismissable: Boolean
-        }
+export default {
+    name: 'Alert',
+    props: {
+        type: String,
+        alertHeading: String,
+        dismissable: Boolean
     }
+}
 </script>
 
 <style lang="scss" scoped>
-    .alert {
-        border: none;
-        border-radius: 0;
+.alert {
+    border: none;
+    border-radius: 0;
 
-        &.alert-success {
-            border-left: solid 6px green;
-        }
+    &.alert-success {
+        border-left: solid 6px green;
     }
+
+    &.alert-danger {
+        border-left: solid 6px #d00000;
+    }
+}
 </style>
