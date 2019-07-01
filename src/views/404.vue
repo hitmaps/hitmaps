9a9c543f95f3a544c5d5d9081937550aed786f24
Mike Koch <Mike.Koch@emerson.com>
2019-07-01 12:48:43 -0400

    Add 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,36 @@
+<template>
+    <div
+        class="content"
+        style="background: url('/img/jpg/backgrounds/loading.jpg') no-repeat center center fixed; background-size: cover"
+    >
+        <header class="row">
+            <div class="col text-center site-header">
+                <h1>Page Not Found</h1>
+            </div>
+        </header>
+        <div class="row text-center">
+            <div class="col">
+                <img src="/img/404.gif" alt="Janus being attacked with muffins"><br>
+                <a style="font-size: .6em; color: white" href="https://www.reddit.com/r/HiTMAN/comments/ai5ao0/death_by_muffins/">u/AJNguyenYT on Reddit</a><br><br>
+                <p>It looks like we couldn't find the page you're looking for. We asked Janus to help, but he wasn't being cooperative, despite being offered several blueberry muffins.</p>
+                <a href="/" style="color: white"><i class="fas fa-arrow-left"></i> Back to the homepage</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: '404',
+    title: 'Page Not Found'
+}
+</script>
+<style lang="scss" scoped>
+    h1 {
+        text-transform: none;
+    }
+    p {
+        color: white;
+    }
+</style>
