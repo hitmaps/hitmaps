83809a26c4a8e541c7ff8194648edead57bbc6ea
Mike Koch <mkoch227@gmail.com>
2019-07-03 21:41:30 -0400

    Trying to split up frontend and backend deploys

--- unzip-build.php
+++ unzip-build.php
@@ -7,6 +7,12 @@ if (!isset($_GET['access-key']) || $_GET['access-key'] !== $settings->accessKey)
     return http_response_code(404);
 }
 
+if ($_GET['zip'] !== 'frontend' && $_GET['zip'] !== 'backend') {
+    return http_response_code(404);
+}
 
-shell_exec('unzip -o latest_build.zip');
-shell_exec('rm latest_build.zip');
+$zip = $_GET['zip'];
+
+
+shell_exec("unzip -o {$zip}.zip");
+shell_exec("rm {$zip}.zip");
