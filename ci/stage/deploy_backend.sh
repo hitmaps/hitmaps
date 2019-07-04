1a0693b0670747f8204cdb83e277ee5556b4ff9a
Mike Koch <mkoch227@gmail.com>
2019-07-03 21:41:30 -0400

    Trying to split up frontend and backend deploys

--- ci/stage/deploy_backend.sh
+++ ci/stage/deploy_backend.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 cd ../
-npm i
-npm run staging
-mkdir latest_build
-TARGETDIR=latest_build;for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
+mkdir backend
+TARGETDIR=backend;for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
 
 # Remove files that we don't want to bundle
-cd latest_build
+cd backend
 rm -rf ci
 rm -rf .git
 rm -rf api/maps
@@ -16,20 +14,14 @@ rm -rf api/maps
 rm api/Config/cachehash.txt
 echo $CI_COMMIT_SHORT_SHA > api/Config/cachehash.txt
 
-cd dist
-mv * ../
+zip -r ../backend.zip . > ../zip_output.txt
 cd ../
-
-ls
-
-zip -r ../latest_build.zip . > ../zip_output.txt
-cd ../
-rm -rf latest_build
-mkdir latest_build
-mv latest_build.zip latest_build
-mv zip_output.txt latest_build
+rm -rf backend
+mkdir backend
+mv backend.zip backend
+mv zip_output.txt backend
 
 lftp -d -e "set ssl:verify-certificate no; mirror -P $NUMBER_OF_CONCURRENT_UPLOADS -Rv $LOCAL_DIR $REMOTE_DIR; quit;" ftp://$STAGE_USERNAME:$PASSWORD@$HOST
 
-wget https://test.hitman2maps.com/unzip-build.php > /dev/null
+wget https://test.hitman2maps.com/unzip-build.php?zip=backend&access-key=$ACCESS_KEY > /dev/null
 wget https://test.hitman2maps.com/admin/migrate?access-key=$ACCESS_KEY > latest_build/phinx-log.txt
