01afb99dd498aed330f0c567c94c6afd219cb09b
Mike Koch <mkoch227@gmail.com>
2019-07-05 15:28:09 -0400

    Change wget to curl

--- ci/stage/deploy_backend.sh
+++ ci/stage/deploy_backend.sh
@@ -23,5 +23,5 @@ mv zip_output.txt backend
 
 lftp -d -e "set ssl:verify-certificate no; mirror -P $NUMBER_OF_CONCURRENT_UPLOADS -Rv backend/ $REMOTE_DIR; quit;" ftp://$STAGE_USERNAME:$PASSWORD@$HOST
 
-wget https://test.hitman2maps.com/unzip-build.php?zip=backend\&access-key=$ACCESS_KEY > /dev/null
-wget https://test.hitman2maps.com/admin/migrate?access-key=$ACCESS_KEY > backend/phinx-log.txt
+curl https://test.hitman2maps.com/unzip-build.php?zip=backend\&access-key=$ACCESS_KEY
+curl https://test.hitman2maps.com/admin/migrate?access-key=$ACCESS_KEY
