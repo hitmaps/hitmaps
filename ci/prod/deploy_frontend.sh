0e39dcf5b0df143e5bda7870e93a4e8f2d0ff642
Mike Koch <mkoch227@gmail.com>
2019-07-16 21:55:12 -0400

    Update CI for production [ci skip]

---
+++ ci/prod/deploy_frontend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../../dist
+
+mkdir frontend
+TARGETDIR=frontend;for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
+
+zip -r ../frontend.zip . > ../zip_output.txt
+cd ../
+rm -rf frontend
+mkdir frontend
+mv frontend.zip frontend
+mv zip_output.txt frontend
+
+lftp -d -e "set ssl:verify-certificate no; mirror -P $NUMBER_OF_CONCURRENT_UPLOADS -Rv frontend/ $REMOTE_DIR; quit;" ftp://$USERNAME:$PASSWORD@$HOST
+
+curl https://www.hitman2maps.com/unzip-build.php?zip=frontend\&access-key=$ACCESS_KEY
