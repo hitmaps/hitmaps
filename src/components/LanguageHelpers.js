496332ee94362b9f99071d592d74ebe3e9156e7e
Mike Koch <mkoch227@gmail.com>
2019-09-06 22:46:28 -0400

    Added language modal... need to store selected language

---
+++ src/components/LanguageHelpers.js
@@ -0,0 +1,83 @@
+export default class {
+    static getLanguages() {
+        return [
+            {
+                iso: 'de-DE',
+                name: 'German'
+            },
+            {
+                iso: 'en-US',
+                name: 'English'
+            },
+            {
+                iso: 'es-ES',
+                name: 'Spanish'
+            },
+            {
+                iso: 'fr-FR',
+                name: 'French'
+            },
+            {
+                iso: 'it-IT',
+                name: 'Italian'
+            },
+            {
+                iso: 'ja-JP',
+                name: 'Japanese'
+            },
+            {
+                iso: 'pl-PL',
+                name: 'Polish'
+            },
+            {
+                iso: 'pt-BR',
+                name: 'Brazilian Portuguese'
+            },
+            {
+                iso: 'ru-RU',
+                name: 'Russian'
+            },
+            {
+                iso: 'zh-CN',
+                name: 'Simplified Chinese'
+            },
+            {
+                iso: 'zh-TW',
+                name: 'Traditional Chinese'
+            }
+        ];
+    }
+
+    static getCountryFlag(language) {
+        switch (language) {
+            case 'de-DE':
+                return 'de';
+            case 'en-US':
+                return 'us';
+            case 'es-ES':
+                return 'es';
+            case 'fr-FR':
+                return 'fr';
+            case 'it-IT':
+                return 'it';
+            case 'ja-JP':
+                return 'jp';
+            case 'pl-PL':
+                return 'pl';
+            case 'pt-BR':
+                return 'br';
+            case 'ru-RU':
+                return 'ru';
+            case 'zh-CN':
+            case 'zh-TW':
+                return 'cn';
+            default:
+                console.error('Failed to find flag for locale: ' + language);
+                return 'us';
+        }
+    }
+
+    static getCountryFlagForLocale(i18n) {
+        return this.getCountryFlag(i18n.locale);
+    }
+}
---
+++ src/components/LocaleModal.vue
@@ -0,0 +1,68 @@
+<template>
+    <modal id="locale-modal"
+        aria-labelledby="locale-modal-label"
+        :modalTitle="$t('language-modal.change-language')"
+        dismissable>
+        <div class="row">
+            <div @click="setLang(lang.iso)" data-dismiss="modal" class="col-md-3 col-sm-6 text-center language-option" v-for="(lang, i) in languages" :key="`Lang${i}`">
+                <country-flag :country="getCountryFlag(lang.iso)" size="big" />
+                <p>{{ lang.name }}</p>
+            </div>
+        </div>
+        <i18n path="language-modal.footer" tag="p">
+            <span slot="submitACorrection">
+                <a href="https://translate.hitman2maps.com" target="_blank">{{ $t('language-modal.submit-a-correction') }}</a>
+            </span>
+        </i18n>
+    </modal>
+</template>
+
+<script>
+    import Modal from "./Modal";
+    import LanguageHelpers from "./LanguageHelpers";
+    import Alert from "./Alert";
+    export default {
+        name: "LocaleModal",
+        components: {Alert, Modal},
+        computed: {
+            languages() {
+                return LanguageHelpers.getLanguages();
+            }
+        },
+        methods: {
+            getCountryFlag(locale) {
+                return LanguageHelpers.getCountryFlag(locale)
+            },
+            setLang(iso) {
+                this.$i18n.locale = iso;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    * {
+        color: #000;
+    }
+
+    a {
+        color: navy;
+
+        &:hover {
+            color: navy;
+        }
+    }
+
+    .notice {
+        margin: 0 10px;
+    }
+
+    .language-option {
+        cursor: pointer;
+        margin-bottom: 10px;
+
+        &:hover {
+            background: rgba(0,0,0,.1);
+        }
+    }
+</style>
