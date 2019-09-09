a981b4733e19b9f15d485acc4eddc7a6bf0bc90e
Mike Koch <mkoch227@gmail.com>
2019-09-09 12:56:54 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -123,12 +123,12 @@
                             <img
                                 src="/img/game-icons/elusive-target.png"
                                 class="normal img-fluid"
-                                :alt="$t('elusive-target-icon'"
+                                :alt="$t('elusive-target-icon')"
                             />
                             <img
                                 src="/img/game-icons/elusive-target-inverted.png"
                                 class="inverted img-fluid"
-                                :alt="$t('elusive-target-icon'"
+                                :alt="$t('elusive-target-icon')"
                             />
                         </div>
                         <div class="text">
