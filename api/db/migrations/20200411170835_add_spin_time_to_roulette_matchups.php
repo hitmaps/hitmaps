f45f31d01e9ee7b609fa0f1b24a62618a7546302
Mike Koch <mkoch227@gmail.com>
2020-04-11 19:02:10 -0400

    Fix default time for existing spins

--- api/db/migrations/20200411170835_add_spin_time_to_roulette_matchups.php
+++ api/db/migrations/20200411170835_add_spin_time_to_roulette_matchups.php
@@ -6,7 +6,7 @@ use Phinx\Migration\AbstractMigration;
 class AddSpinTimeToRouletteMatchups extends AbstractMigration {
     public function change() {
         $this->table('roulette_matchups')
-            ->addColumn('spin_time', 'datetime')
+            ->addColumn('spin_time', 'datetime', ['default' => '2020-04-01 00:00:00.000'])
             ->update();
     }
 }
