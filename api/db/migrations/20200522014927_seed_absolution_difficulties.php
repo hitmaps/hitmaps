ece7b19e9c8821f8ebbbc12ab75e8ff7e9f0fa67
Mike Koch <mkoch227@gmail.com>
2020-07-07 22:10:57 -0400

    Update folder for absolution stuff

--- api/db/migrations/20200522014927_seed_absolution_difficulties.php
+++ api/db/migrations/20200522014927_seed_absolution_difficulties.php
@@ -50,141 +50,141 @@ class SeedAbsolutionDifficulties extends AbstractMigration {
                 'slug' => 'a-personal-contract',
                 'location' => 19,
                 'order' => 1,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/a-personal-contract-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/a-personal-contract-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'The King of Chinatown' => [
                 'slug' => 'the-king-of-chinatown',
                 'location' => 19,
                 'order' => 2,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/the-king-of-chinatown-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/the-king-of-chinatown-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Terminus' => [
                 'slug' => 'terminus',
                 'location' => 19,
                 'order' => 3,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/terminus-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/terminus-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Run For Your Life' => [
                 'slug' => 'run-for-your-life',
                 'location' => 19,
                 'order' => 4,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/run-for-your-life-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/run-for-your-life-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Hunter and Hunted' => [
                 'slug' => 'hunter-and-hunted',
                 'location' => 19,
                 'order' => 5,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/hunter-and-hunted-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/hunter-and-hunted-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Rosewood' => [
                 'slug' => 'rosewood',
                 'location' => 19,
                 'order' => 6,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/rosewood-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/rosewood-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Welcome to Hope' => [
                 'slug' => 'welcome-to-hope',
                 'location' => 20,
                 'order' => 1,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/welcome-to-hope-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/welcome-to-hope-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             "Birdie's Gift" => [
                 'slug' => 'birdies-gift',
                 'location' => 20,
                 'order' => 2,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/birdies-gift-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/birdies-gift-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Shaving Lenny' => [
                 'slug' => 'shaving-lenny',
                 'location' => 20,
                 'order' => 3,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/shaving-lenny-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/shaving-lenny-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'End of the Road' => [
                 'slug' => 'end-of-the-road',
                 'location' => 20,
                 'order' => 4,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/end-of-the-road-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/end-of-the-road-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Dexter Industries' => [
                 'slug' => 'dexter-industries',
                 'location' => 20,
                 'order' => 5,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/dexter-industries-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/dexter-industries-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Death Factory' => [
                 'slug' => 'death-factory',
                 'location' => 20,
                 'order' => 6,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/death-factory-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/death-factory-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Fight Night' => [
                 'slug' => 'fight-night',
                 'location' => 20,
                 'order' => 7,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/fight-night-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/fight-night-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Attack of the Saints' => [
                 'slug' => 'attack-of-the-saints',
                 'location' => 20,
                 'order' => 8,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/attack-of-the-saints-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/attack-of-the-saints-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             "Skurky's Law" => [
                 'slug' => 'skurkys-law',
                 'location' => 20,
                 'order' => 9,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/skurkys-law-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/skurkys-law-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'Operation Sledgehammer' => [
                 'slug' => 'operation-sledgehammer',
                 'location' => 20,
                 'order' => 10,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/operation-sledgehammer-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-hope.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/operation-sledgehammer-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-hope.jpg'
             ],
             'One of a Kind' => [
                 'slug' => 'one-of-a-kind',
                 'location' => 21,
                 'order' => 1,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/one-of-a-kind-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/one-of-a-kind-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Blackwater Park' => [
                 'slug' => 'blackwater-park',
                 'location' => 21,
                 'order' => 2,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/blackwater-park-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/blackwater-park-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Countdown' => [
                 'slug' => 'countdown',
                 'location' => 21,
                 'order' => 3,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/countdown-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-chicago.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/countdown-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/absolution-chicago.jpg'
             ],
             'Absolution' => [
                 'slug' => 'absolution',
                 'location' => 22,
                 'order' => 1,
-                'tileUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/absolution-1.jpg',
-                'backgroundUrl' => 'https://media.hitmaps.com/img/hitmaps-custom/hitman-absolution.jpg'
+                'tileUrl' => 'https://media.hitmaps.com/img/absolution/absolution-1.jpg',
+                'backgroundUrl' => 'https://media.hitmaps.com/img/absolution/hitman-absolution.jpg'
             ]
         ];
 
