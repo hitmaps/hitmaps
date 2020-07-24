0f6aaf3e3f7d2c236a3f16488fe5cf1125ba2e47
Mike Koch <mkoch227@gmail.com>
2020-07-23 22:18:07 -0400

    Seeded absolution language keys for custom floor names

--- api/db/migrations/20200722020109_add_map_level_to_name_table.php
+++ api/db/migrations/20200722020109_add_map_level_to_name_table.php
@@ -7,6 +7,7 @@ class AddMapLevelToNameTable extends AbstractMigration {
     public function change() {
         $this->table('map_floor_to_name')
             ->addColumn('mission_id', 'integer')
+            ->addColumn('floor_number', 'integer')
             ->addColumn('name_key', 'string')
             ->create();
     }
