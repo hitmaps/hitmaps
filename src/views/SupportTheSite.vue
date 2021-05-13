07a4ae2f7424dd0587577791fe088d9ef8970cb2
Mike Koch <mkoch227@gmail.com>
2021-05-13 10:47:02 -0400

    Update current list of patrons

--- src/views/SupportTheSite.vue
+++ src/views/SupportTheSite.vue
@@ -34,6 +34,15 @@
                         <patron name="Some Random Person" tier="Assassin"/>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <patron name="ChromeX" tier="Assassin" />
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-3">
+                        <patron name="Mikulers" tier="Assassin" />
+                    </div>
+                </div>
             </div>
             <h3 style="margin-top: 20px;">PayPal</h3>
             <p>
