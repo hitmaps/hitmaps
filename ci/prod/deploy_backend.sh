cbfa97631469bf61d2af3c968ee7e1d4f066e8ae
Mike Koch <mkoch227@gmail.com>
2021-05-20 21:39:04 -0400

    Remove extra unnecessary files

--- ci/prod/deploy_backend.sh
+++ ci/prod/deploy_backend.sh
@@ -9,6 +9,29 @@ cd backend
 rm -rf ci
 rm -rf .git
 rm -rf api/maps/
+rm -rf js
+rm -rf "Raw Media"
+rm -rf scss
+rm buildtiles.bat
+rm crowdin.yml
+rm gdal2tiles.py
+rm LICENSE
+rm README.md
+
+# Remove Vue stuff, as this is the backend
+rm -rf src
+rm -rf public
+rm .browserslistrc
+rm .env
+rm .env.development
+rm .env.staging
+rm .gitignore
+rm .prettierrc
+rm babel.config.js
+rm vue.config.js
+rm package.json
+rm package-lock.json
+rm postcss.config.js
 
 zip -r ../backend.zip . > ../zip_output.txt
 cd ../
