1541367f2ff805937faa7c2913cd92f046fcb9ce
Mike Koch <mkoch227@gmail.com>
2021-06-06 20:21:55 -0400

    Fix script

--- ci/prod/deploy_frontend.sh
+++ ci/prod/deploy_frontend.sh
@@ -13,7 +13,7 @@ mv frontend.zip frontend
 mv zip_output.txt frontend
 
 cd frontend
-scp -r frontend/frontend.zip $USERNAME@$HOST:$PROD_UPLOAD_DIR/frontend.zip
-scp -r frontend/zip_output.txt $USERNAME@$HOST:PROD_UPLOAD_DIR/zip_output.txt
+scp -r frontend.zip $USERNAME@$HOST:$PROD_UPLOAD_DIR/frontend.zip
+scp -r zip_output.txt $USERNAME@$HOST:PROD_UPLOAD_DIR/zip_output.txt
 
 #curl https://www.hitmaps.com/unzip-build.php?zip=frontend\&access-key=$ACCESS_KEY
