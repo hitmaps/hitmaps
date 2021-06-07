b9d58d3c645911652aa9d2bb69fe2fa76657f073
Mike Koch <mkoch227@gmail.com>
2021-06-06 20:35:47 -0400

    Disable host key verification checks

--- ci/prod/deploy_frontend.sh
+++ ci/prod/deploy_frontend.sh
@@ -14,8 +14,8 @@ mv zip_output.txt frontend
 
 cd frontend
 echo 'Uploading frontend.zip'
-scp -r frontend.zip $USERNAME@$HOST:$PROD_UPLOAD_DIR/frontend.zip
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r frontend.zip $USERNAME@$HOST:$PROD_UPLOAD_DIR/frontend.zip
 echo 'Uploading zip_output.txt'
-scp -r zip_output.txt $USERNAME@$HOST:$PROD_UPLOAD_DIR/zip_output.txt
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r zip_output.txt $USERNAME@$HOST:$PROD_UPLOAD_DIR/zip_output.txt
 
 curl https://www.hitmaps.com/unzip-build.php?zip=frontend\&access-key=$ACCESS_KEY
