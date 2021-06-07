66fc519d4de3d74957abfcbd4f0c0c69ba524b84
Mike Koch <mkoch227@gmail.com>
2021-06-06 20:29:42 -0400

    Uncomment curl command to unzip the build

--- ci/prod/deploy_frontend.sh
+++ ci/prod/deploy_frontend.sh
@@ -18,4 +18,4 @@ scp -r frontend.zip $USERNAME@$HOST:$PROD_UPLOAD_DIR/frontend.zip
 echo 'Uploading zip_output.txt'
 scp -r zip_output.txt $USERNAME@$HOST:$PROD_UPLOAD_DIR/zip_output.txt
 
-#curl https://www.hitmaps.com/unzip-build.php?zip=frontend\&access-key=$ACCESS_KEY
+curl https://www.hitmaps.com/unzip-build.php?zip=frontend\&access-key=$ACCESS_KEY
