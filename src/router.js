2922bae9183d996eac4d8c9948c5f6d900e50f99
Mike Koch <mkoch227@gmail.com>
2021-12-27 22:40:49 -0500

    Remove login page
    All it did was add an extra, unnecessary step to authenticating

--- src/router.js
+++ src/router.js
@@ -65,15 +65,6 @@ export default new Router({
                 import(/* webpackChunkName: "map-view" */ './views/Map.vue'),
         },
         {
-            path: '/user/login',
-            name: 'login',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "login" */ './views/Login.vue'),
-        },
-        {
             path: '/auth',
             name: 'auth',
             // route level code-splitting
