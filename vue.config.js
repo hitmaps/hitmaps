10e905db3477f669dcbccf5918a0f27e9cc4141d
Mike Koch <mkoch227@gmail.com>
2023-07-01 09:24:16 -0400

    Attempting to fix the build lel

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin');
-
 module.exports = {
   css: {
     loaderOptions: {
@@ -17,12 +15,5 @@ module.exports = {
     }
   },
   configureWebpack: {
-    plugins: [
-      new RollbarSourceMapPlugin({
-        accessToken: '78c7dc0eff9f44a8a460396dd6ba4dd1',
-        version: 'version-1',
-        publicPath: 'https://www.hitmaps.com'
-      })
-    ]
   }
 }
