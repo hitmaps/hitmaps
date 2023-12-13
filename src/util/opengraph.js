0363037f4ff3390366e4c5df8bfec11f32ac16f1
Mike Koch <mkoch227@gmail.com>
2023-12-12 23:03:30 -0500

    Clean up old Vue files

--- src/util/opengraph.js
+++
@@ -1,8 +0,0 @@
-import MetaHandler from "../components/MetaHandler.js";
-
-export default {
-    mounted() {
-        MetaHandler.setOpengraphTag('url', document.location);
-        MetaHandler.setTwitterTag('domain', window.location.hostname.replace('www.', ''));
-    },
-}
